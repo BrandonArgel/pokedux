f0f44689b15df3febb41494ea56a2437c9e2e091
BrandonArgel <brandargel@gmail.com>
2023-06-28 20:45:52 -0600

    :ambulance: Add remaining props for Card component
    Message

--- src/containers/PokemonList/index.tsx
+++ src/containers/PokemonList/index.tsx
@@ -7,10 +7,13 @@ type PokemonListProps = {
 };
 
 const defaultPokemon = {
+	id: 1,
 	name: "bulbasaur",
 	image:
 		"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 	types: ["grass", "poison"],
+	isFavorite: false,
+	handleFavorite: () => undefined,
 };
 
 export const PokemonList: React.FC<PokemonListProps> = ({ children, loading }) => {
