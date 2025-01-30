ed434cac5fefdba7397890b9b016cb4e38bf822a
BrandonArgel <brandargel@gmail.com>
2025-01-30 08:55:49 -0600

    🚀 Upgrade dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,25 @@
 module.exports = {
-	env: { browser: true, es2020: true, node: true },
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react-hooks/recommended",
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-	plugins: ["react-refresh"],
-	rules: {
-		"@typescript-eslint/no-explicit-any": ["off"],
-		"@typescript-eslint/no-non-null-assertion": ["off"],
-		"react-refresh/only-export-components": ["off"],
-	},
-	globals: {
-		window: true,
-		document: true,
-		localStorage: true,
-		FormData: true,
-		FileReader: true,
-		Blob: true,
-		navigator: true,
-	},
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/no-non-null-assertion": ["off"],
+    "react-refresh/only-export-components": ["off"],
+  },
+  globals: {
+    window: true,
+    document: true,
+    localStorage: true,
+    FormData: true,
+    FileReader: true,
+    Blob: true,
+    navigator: true,
+  },
 };
